511b94787452b362a894feca6ac7113bdb709803
YuxinZhang <taylorzhangyx@gmail.com>
2020-06-25 21:20:17 -0700

    add the binary tree definations
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

--- knowNotes/BinaryTree.md
+++ knowNotes/BinaryTree.md
@@ -1,11 +1,17 @@
-Binary Tree
+# Binary Tree
+a binary tree is a tree data structure in which each node has at most two children, which are referred to as the left child and the right child.
 
-Full Binary Tree
+## Full Binary Tree
+A full binary tree (sometimes referred to as a proper or plane binary tree) is a tree in which every node has either 0 or 2 children.
 
-Complete Binary Tree
+## Complete Binary Tree
+In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. A complete binary tree can be efficiently represented using an array.
 
-Degenerate Binary Tree
+## Degenerate Binary Tree
+A degenerate (or pathological) tree is where each parent node has only one associated child node.[24] This means that the tree will behave like a linked list data structure.
 
-Perfect Binary Tree
+## Perfect Binary Tree
+A perfect binary tree is a binary tree in which all interior nodes have two children and all leaves have the same depth or same level.
 
-Balanced Binary Tree
+## Balanced Binary Tree
+A balanced binary tree is a binary tree structure in which the left and right subtrees of every node differ in height by no more than 1.
--- techChanllenges/005.MaxWaterTrapped.py
+++ techChanllenges/005.MaxWaterTrapped.py
@@ -2,45 +2,44 @@
 # Given a non-negative integer array representing the heights of a list of adjacent bars. Suppose each bar has a width of 1. Find the largest amount of water that can be trapped in the histogram.
 
 # Assumptions
-
 # The given array is not null
 # Examples
-
 # { 2, 1, 3, 2, 4 }, the amount of water can be trapped is 1 + 1 = 2 (at index 1, 1 unit of water can be trapped and index 3, 1 unit of water can be trapped)
 
+
 class Solution(object):
-  def maxTrapped(self, array):
-    """
+    def maxTrapped(self, array):
+        """
     input: int[] array
     return: int
     """
-    # write your solution here
-    # start checking
-    if len(array) <= 2:
-      return 0
+        # write your solution here
+        # start checking
+        if len(array) <= 2:
+            return 0
 
-    # init
-    water = 0
-    left = 0
-    que = list()
+        # init
+        water = 0
+        left = 0
+        que = list()
 
-    def calculateWater(h):
-      waterT = 0
-      right = h
-      while len(que) != 0:
-        curH = array[que.pop()]
-        diff = min(left, right) - curH
-        waterT += diff if diff > 0 else 0
-        right = max(h, curH)
-      return waterT
+        def calculateWater(h):
+            waterT = 0
+            right = h
+            while len(que) != 0:
+                curH = array[que.pop()]
+                diff = min(left, right) - curH
+                waterT += diff if diff > 0 else 0
+                right = max(h, curH)
+            return waterT
 
-    for i in range(len(array)):
-      curr = array[i]
-      if curr < left:
-        que.append(i)
-      else:
-        water += calculateWater(curr)
-        left = curr
+        for i in range(len(array)):
+            curr = array[i]
+            if curr < left:
+                que.append(i)
+            else:
+                water += calculateWater(curr)
+                left = curr
 
-    water += calculateWater(0)
-    return water
+        water += calculateWater(0)
+        return water
