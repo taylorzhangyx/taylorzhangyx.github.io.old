5d20847f8032aa2d298cca8c41fe97b93e1466f6
YuxinZhang <taylorzhangyx@gmail.com>
2020-06-26 10:21:35 -0700

    add notes and format
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

--- techChanllenges/006.OneEditDistance.py
+++ techChanllenges/006.OneEditDistance.py
@@ -1,36 +1,41 @@
+# Note:
+# 1. when match, move 2 pointers
+# 2. when not match, the longer one is the one need to be skipped.
+
+
 class Solution(object):
-  def oneEditDistance(self, source, target):
-    """
+    def oneEditDistance(self, source, target):
+        """
     input: string source, string target
     return: boolean
     """
-    # write your solution here
-    ls = len(source)
-    lt = len(target)
-    if lt > ls:
-      return self.oneEditDistance(target, source)
+        # write your solution here
+        ls = len(source)
+        lt = len(target)
+        if lt > ls:
+            return self.oneEditDistance(target, source)
 
-    if abs(ls - lt) > 1:
-      return False
-    diffCount = 0
+        if abs(ls - lt) > 1:
+            return False
+        diffCount = 0
 
-    si = ls-1
-    ti = lt-1
-    while ti >= 0 and si>=0:
-      # compare the char at si
-      if source[si] == target[ti]:
-        si -= 1
-        ti -= 1
-        continue
-      else:
-        diffCount += 1
-        if diffCount > 1:
-          return False
-        if si > ti:
-          si-=1
-        elif ti > si:
-          ti-=1
-        else:
-          si-=1
-          ti-=1
-    return diffCount == 1 or (diffCount == 0 and abs(ti - si)==1)
+        si = ls - 1
+        ti = lt - 1
+        while ti >= 0 and si >= 0:
+            # compare the char at si
+            if source[si] == target[ti]:
+                si -= 1
+                ti -= 1
+                continue
+            else:
+                diffCount += 1
+                if diffCount > 1:
+                    return False
+                if si > ti:
+                    si -= 1
+                elif ti > si:
+                    ti -= 1
+                else:
+                    si -= 1
+                    ti -= 1
+        return diffCount == 1 or (diffCount == 0 and abs(ti - si) == 1)
