f37b70d908726f4e2e92223428261497579b74d7
root <tayzhang@tencent.com>
2022-03-07 19:31:34 +0800

    add blog code sample
    Signed-off-by: root <tayzhang@tencent.com>

---
+++ react_nextjs/nextjs_sample/nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import Link from "next/link";
+import Script from "next/script";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title> first post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </Layout>
+  );
+}
---
+++ react_nextjs/nextjs_sample/nextjs-blog/postcss.config.js
@@ -0,0 +1,13 @@
+// postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // For the best performance and to avoid false positives,
+    // be as specific as possible with your content configuration.
+  ],
+};
