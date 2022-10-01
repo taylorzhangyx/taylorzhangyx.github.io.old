ce404275f671824b7cc9fda3c97aeb0a1d681ed4
tayzhang <tayzhang@tencent.com>
2022-10-01 15:14:01 +0800

    update
    Signed-off-by: tayzhang <tayzhang@tencent.com>

--- golang/hash/main2.go
+++ golang/hash/main2.go
@@ -12,16 +12,16 @@ var key_text = "astaxie12798akljzmknm.ahkjkljl;k"
 var key_text_front = "se2axie62793akljzm34m.ahkjk5jl;f"
 var commonIV = []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
 
-//EncryptAES
+// EncryptAES
 func EncryptAES(text string, key string) (string, error) {
-	//aes的加密字符串
+	// aes的加密字符串
 
 	c, err := aes.NewCipher([]byte(key))
 	if err != nil {
 		return "", errors.New(fmt.Sprintf("Error: NewCipher(%d bytes) = %s", len(key), err))
 	}
 
-	//加密字符串
+	// 加密字符串
 	cfb := cipher.NewCFBEncrypter(c, commonIV)
 	ciphertext := make([]byte, len(text))
 	cfb.XORKeyStream(ciphertext, []byte(text))
@@ -51,13 +51,16 @@ func EncryptPassword(ticket string) (string, error) {
 }
 
 func main() {
-	//if len(os.Args) != 2 {
+	// if len(os.Args) != 2 {
 	//	fmt.Printf("usage ./encrypt_password DecodePassword \n")
 	//	return
-	//}
-	//input := os.Args[1]
+	// }
+	// input := os.Args[1]
 	input := "1qaz!QAZ"
 	encode, _ := EncryptPassword(input)
 	fmt.Printf("decode password result [%s]\n", encode)
 
+	m := make(map[string]string)
+	m["1"] = "111"
+	fmt.Printf("empty=%s", m["1"])
 }
